234ab213c9b3bd9c7d18be4f6370502548b7dde3
Wolfgang Busch <wolfgang@busch-dettum.de>
2019-03-05 11:30:17 +0100

    Add files via upload

--- install.php
+++ install.php
@@ -8,6 +8,6 @@
 require_once __DIR__.'/lib/class.page_counter_install.php';
 #
 # --- Einfuegen der Aufrufzaehler-Spalte in rex_article, Erzeugen des Zaehler-Moduls
-page_counter_install::counter_insert_counter_column();
-page_counter_install::counter_build_module($this->getPackageId());
+page_counter_install::insert_counter_column();
+page_counter_install::build_module($this->getPackageId());
 ?>
